e1b69c6c21fc18bff7112f497b83b2c6519aa385
priyankaksolves <priyanka.netha@gmail.com>
2025-01-02 19:28:22 +0530

    error fix

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { BooksState } from '../redux/bookSlice';
+import { Book } from '../redux/bookSlice';
+import './BookList.module.css';  // Make sure the CSS file is correctly imported
 
 const BookList: React.FC = () => {
-  const books = useSelector((state: BooksState) => state.books); // Select the books from the Redux store
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const books = useSelector((state: any) => {
+    console.log(state); // This is a side effect (it prints the state, but doesn't return anything)
+    return state.books?.books; // This is an expression that doesn’t do anything; it’s not returning anything
+  });
 
   if (books.length === 0) {
-    return <div>No books found</div>;
+    return <div className="noBooks">No books found</div>; // Display when there are no books
   }
 
   return (
-    <div>
+    <div className="container">
       <h2>Books List</h2>
       <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <h3>{book.volumeInfo.title}</h3>
-            <p>{book.volumeInfo.description}</p>
-            <p><strong>Authors:</strong> {book.volumeInfo.authors.join(', ')}</p>
+        {books.map((book: Book) => (
+          <li key={book?.id}>
+            <h3>{book?.volumeInfo?.title}</h3>
+            <p>{book?.volumeInfo?.description}</p>
+            <p>
+              <strong>Authors:</strong> {book?.volumeInfo?.authors?.join(', ')}
+            </p>
           </li>
         ))}
       </ul>
