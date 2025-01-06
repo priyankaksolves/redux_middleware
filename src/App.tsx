2009f85c1e4054cd00bb7404126c4a831fffa183
priyankaksolves <priyanka.netha@gmail.com>
2025-01-06 15:25:47 +0530

    about component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Search from './components/Search';
 import BookList from './components/BookList';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import About from './components/about';
 
 const App: React.FC = () => {
   return (
+ <>
     <div>
       <h1>Google Book Store</h1>
       <Search />
       <BookList />
+
     </div>
+    <div>
+          <BrowserRouter>
+          <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+            <Link to="/about">About</Link>
+          </nav>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+    </div></>
+
   );
 };
 
